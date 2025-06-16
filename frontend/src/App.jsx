ae244b16115b7bb8dda215ebfc5994457999f463
Lalatendu Bhatta <lalatendubhatta@gmail.com>
2025-06-16 11:06:56 +0530

    ui configuration and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
+import Employee from "./pages/Employee"
+import Admin from "./pages/Admin"
+import ErrorPage from "./pages/ErrorPage"
+
 function App() {
   return (
-    <div className="bg-red-500">App</div>
+    <>
+      <Routes>
+        {/* <Route path="/" element={ } /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/employee" element={<Employee />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
   )
 }
 export default App
